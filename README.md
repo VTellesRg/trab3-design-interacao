2cfd3948c48665c84aa6ca3f9cb4b10f4db055ad
VTellesRg <vini.m.telles@gmail.com>
2024-08-17 13:58:50 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# trab3-design-interacao
+# Trab-desgn-interacao-3
+Repositório criado para o trabalho final da disciplina Design de interacao IFRS - Rio Grande
+
+
+endereço https://vtellesrg.github.io/Trab-desgn-interacao-3/
+
+
+ideia de pagina: site de venda de carros
+primeiro exercicio: 2 consultas permanentes na tela, site .br cadastrado e oficial
+cnpj ativo e verificado 
+
+consulta do usuario: tabela fipe e bancos que aceitam pix
+
+
+segundo exercicio: 
---
+++ scripts.js
@@ -0,0 +1,34 @@
+// Mostrar/esconder o botão "Voltar ao Topo" conforme o usuário rola a página
+window.addEventListener('scroll', function () {
+	const backToTopButton = document.querySelector('.back-to-top');
+	if (window.scrollY > 200) {
+		backToTopButton.style.display = 'block';
+	} else {
+		backToTopButton.style.display = 'none';
+	}
+});
+
+// Função para retornar suavemente ao topo ao clicar no botão
+document.addEventListener('DOMContentLoaded', function () {
+	document.querySelector('.back-to-top').addEventListener('click', function (e) {
+		e.preventDefault();
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth'
+		});
+	});
+});
+// para todas as consultas a api realizar try cath
+// section 1 - consulta ao brasil api
+// primeira consulta a 2 apis sempre mostrando na tela essas 2 consultas
+// 2 primeiras consultas: site verificado meu carro novo
+// e cnpj valido meu carro novo 41.030.394/0001-08
+
+// segunda consulta a 2 apis porem recebendo 2 parametros de formularios do usuario
+// parametros de consulta: nome do carro na FIPE ver preco
+// segundo form: consulte o codigo do seu banco para verificar financiamento
+//section 2 - consulta a api de sua escolha via 2 botoes
+// cada botão retorna consultas em 3 apis distintas
+//primeiro botao usando promise.race para retornar a primeira consulta que retornar
+//segundo botao usando promise.all para retornar todas as consultas ou mensagem de erro
+// em todas as consultas tratar erros com fetch, contornar possiveis erros do usuario
